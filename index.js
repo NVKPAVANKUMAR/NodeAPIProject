67f0e078342963faec071de1086b4c74ed5ab090
Pavan Kumar <npavankumar@deloitte.com>
2020-06-13 20:21:38 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,28 @@
+const express = require("express");
+let dataFile = require('./users.json');
+const app = express();
+
+app.get('/api/users', (req, res) => {
+    res.send(dataFile);
+})
+
+app.get('/api/info', (res) => {
+    res.send("Welcome to Nodejs API..!");
+});
+
+app.get('/api/users/:id', (req, res) => {
+    const id = req.params.id;
+    var result = dataFile.data.filter((chain) => {
+        return chain.id == id;
+    })[0];
+   
+    if (result) {
+        res.send(result);
+    } else {
+         res.send('this user is not found');
+    }
+});
+
+app.listen(PORT = 5000, () => {
+    console.log('Server is started, please call API Port no - ', PORT);
+});

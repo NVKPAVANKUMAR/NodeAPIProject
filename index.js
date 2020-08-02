6617cfdd9c450fc7976a34619c603a3da9f6facc
Pavan Kumar <npavankumar@deloitte.com>
2020-08-02 15:41:29 +0530

    d-c.yml added.

--- index.js
+++ index.js
@@ -1,33 +1,17 @@
 const express = require("express");
-let dataFile = require('./users.json');
 const app = express();
 
 
-app.get('/',(req,res) =>{
+app.get('/', (req, res) => {
     res.send("Hello Node-Express!")
 })
 
-app.get('/api/users', (req, res) => {
-    res.send(dataFile);
-})
 
-app.get('/api/info', (req,res) => {
-    res.send("Welcome to Nodejs API..!");
+app.get('/api/info', (req, res) => {
+    res.send("Welcome to Nodejs Express API..!");
 });
 
-app.get('/api/users/:id', (req, res) => {
-    const id = req.params.id;
-    var result = dataFile.data.filter((chain) => {
-        return chain.id == id;
-    })[0];
-   
-    if (result) {
-        res.send(result);
-    } else {
-         res.send('this user is not found');
-    }
-});
 
-app.listen(PORT =  process.env.PORT || 5000, () => {
+app.listen(PORT = process.env.PORT || 5000, () => {
     console.log('Server is started, please call API Port no - ', PORT);
 });
